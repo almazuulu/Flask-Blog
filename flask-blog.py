7e86b5eb91a7002eb9ddd4db9f5a82f043ed1c92
almazuulu <almazuulu@gmail.com>
2022-06-11 16:31:55 +0600

    Created template pages

--- flask-blog.py
+++ flask-blog.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Askarbek Almazbek uulu',
+        'title': 'My first blog',
+        'content': 'First blog was created',
+        'date_posted': 'June 12, 2022'
+    },
+
+    {
+        'author': 'Adilet Eshmamatov',
+        'title': 'My second blog',
+        'content': 'Second blog is awesome',
+        'date_posted': 'June 13, 2022'
+    },
+
+]
 
 @app.route("/")
 @app.route("/home")
 def hello():
-    return "<h1>Main Page!!</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about_page():
-    return "<h1>About page</h1>"
+    return render_template('about.html', title = 'About page')
 
 if __name__ == '__main__':
     app.run(debug=True)
